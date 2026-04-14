20eca0d4c4c42541e2016ff654a88afd46b468ec
diixo <anatolii.podoba@gmail.com>
2026-04-15 02:32:50 +0300

    added dashboard (draft template)

--- ui/app_main/views.py
+++ ui/app_main/views.py
@@ -7,4 +7,10 @@ def main(request):
         "title": "AIdeex",
         "description": "AIdeex description"})
 
+def dashboard(request):
+
+    return render(request, "app_main/dashboard.html", context={
+        "title": "AIdeex - Dashboard",
+        "description": "AIdeex dashboard description"})
+
 # Create your views here.
