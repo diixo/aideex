1f3a1fada14afbe74977bd1d16802e23cc16a66f
diixo <anatolii.podoba@gmail.com>
2026-04-15 16:00:53 +0300

    base.html styling adaptation, confluence.html with route added

--- ui/app_main/views.py
+++ ui/app_main/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def main(request):
-
+    return redirect(to="app_main:confluence")
     return render(request, "app_main/index.html", context={
         "title": "AIdeex",
         "description": "AIdeex description"})
@@ -12,5 +12,11 @@ def dashboard(request):
     return render(request, "app_main/dashboard.html", context={
         "title": "AIdeex - Dashboard",
         "description": "AIdeex dashboard description"})
+
+
+def confluence(request):
+    return render(request, "app_main/confluence.html", context={
+        "title": "AIdeex - Confluence",
+        "description": "AIdeex confluence description"})
 
 # Create your views here.
